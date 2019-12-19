651e8612dfb4fb54312f6e32e0346f917ceb4d7b
koron0902 <koron0902@gmail.com>
2019-12-19 17:41:10 +0900

    AudioExtentionsをビルドしてリンクするようにした

--- src/cocoa/dummy.cc
+++ src/cocoa/dummy.cc
@@ -1,13 +1,13 @@
-//#include "AudioDevice.h"
-//#include "Decoders.h"
-//#include "Encoders.h"
+#include "AudioDevice.h"
+#include "Decoders.h"
+#include "Encoders.h"
 #include "dummy.h"
 
-//using namespace nqr;
+using namespace nqr;
 void dummy(){
-/*	AudioDevice::ListAudioDevices();
+	AudioDevice::ListAudioDevices();
 	const int desiredSampleRate = 44100;
 	const int desiredChannelCount = 2;
 	AudioDevice myDevice(desiredChannelCount, desiredSampleRate);
 	myDevice.Open(myDevice.info.id);
-*/}
+}
